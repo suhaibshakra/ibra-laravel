226898e165bb01389039218381931df9fedc5465
Suhaib Shakra <s.shakra@yahoo.com>
2023-03-26 23:15:07 +0300

    speed cars1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Speed Cars</title>
+<title>Speed Cars1</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="w3.css">
